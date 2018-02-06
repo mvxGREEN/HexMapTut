5b402cbaac0f7a464ba0b9173ce46b13ba1196a1
xxxgreen <mvxgreen@gmail.com>
2018-02-06 02:38:15 -0600

    position cells for a hex map

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -30,10 +30,11 @@ public class HexGrid : MonoBehaviour {
 	}
 
 	void CreateCell (int x, int z, int i) {
+		//define position
 		Vector3 position;
-		position.x = x * 10f;
+		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
 		position.y = 0f;
-		position.z = z * 10f;
+		position.z = z * (HexMetrics.innerRadius * 1.5f);
 
 		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
 		cell.transform.SetParent(transform, false);
